0f7cfd70138ce5ce74c571a8d36de4f18fc7a93e
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2024-04-19 13:45:30 +0500

    add/appointments page

--- src/app/components/pages/account/orders/OrdersNav.tsx
+++ src/app/components/pages/account/orders/OrdersNav.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import LinksListOnMobile from '../policy/LinksListPolicyOnMobile';
+import LinksListOnMobile from '../orders/LinksListOnMobile';
 
 interface Link {
   name: string;

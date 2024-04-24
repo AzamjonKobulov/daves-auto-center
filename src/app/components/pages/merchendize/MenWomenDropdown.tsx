17ac5404de9ebe4796832b46726660296990be88
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2024-04-25 00:29:56 +0500

    fix/some issues

--- src/app/components/pages/merchendize/MenWomenDropdown.tsx
+++ src/app/components/pages/merchendize/MenWomenDropdown.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MenWomenDropdown = () => {
   return (
-    <div className="w-40 sm:w-80 md:w-96 lg:w-[29.375rem] grid sm:grid-cols-2 gap-5 sm:gap-7 lg:gap-10 bg-white text-sm sm:text-base md:text-lg lg:text-xl py-[1.875rem] rounded-10 sm:rounded-20 shadow-privacy px-5 sm:px-6 lg:px-10">
+    <div className="w-40 sm:w-80 md:w-96 lg:w-[29.375rem] grid sm:grid-cols-2 gap-5 sm:gap-7 lg:gap-10 bg-white text-sm sm:text-base md:text-lg lg:text-xl py-5 sm:py-[1.875rem] rounded-10 sm:rounded-20 shadow-privacy px-5 sm:px-6 lg:px-10">
       <div className="space-y-3.5">
         <p className="font-bold text-brand-dark border-b border-brand-gray pb-3.5">
           Men

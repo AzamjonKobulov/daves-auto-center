207d5a3df6a15260582afb8317cc3d599eaf3628
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2024-04-25 00:19:23 +0500

    add/delete cross button to the item in the cart

--- src/app/components/shared/CartProduct.tsx
+++ src/app/components/shared/CartProduct.tsx
@@ -39,7 +39,25 @@ const CartProduct = () => {
             </div>
           </div>
         </div>
-        <p className="text-17 pb-3 sm:pb-0">$35.00</p>
+        <div className="flex flex-col items-end gap-7 xl:gap-4">
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+          <p className="text-17 pb-3 sm:pb-0">$35.00</p>
+        </div>
       </div>
     </div>
   );
